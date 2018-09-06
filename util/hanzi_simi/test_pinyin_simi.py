80cd22c635b4ce97c3e62f49691f32449a3a572c
Zhaoqun Chen <zhaoqun.czq@antfin.com>
2018-09-06 17:17:35 +0800

    add test case

--- util/hanzi_simi/test_pinyin_simi.py
+++ util/hanzi_simi/test_pinyin_simi.py
@@ -10,7 +10,10 @@ def test():
 
   simiThreshold = 0.9
 
-  testCases = [[u'杨', u'扬', True],[u'杨', u'验', True],[u'转', u'钻', True],[u'装', u'钻', False]]
+  testCases = [
+    [u'杨', u'扬', True], [u'杨', u'验', True], [u'转', u'钻', True], 
+    [u'劈', u'辟', True], [u'装', u'钻', False], [u'装', u'呀', False]
+  ]
   for [char1, char2, simiFlag] in testCases:
     simi = ps.simi(char1, char2)
     print '%s %s %2.2f' %(char1, char2, simi)
