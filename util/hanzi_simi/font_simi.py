2b17a22853e9061e3171af4494fcb246c674b008
Zhaoqun Chen <zhaoqun.czq@antfin.com>
2018-09-06 16:45:07 +0800

    init

---
+++ util/hanzi_simi/font_simi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-09-06 16:43:40
+# @Author  : zhaoqun.czq
+# calculate similarity based on character shape
+
+import os
---
+++ util/hanzi_simi/pinyin_simi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-09-06 16:44:06
+# @Author  : zhaoqun.czq
+# calculate similarity based on character pinyin
+
+import os
